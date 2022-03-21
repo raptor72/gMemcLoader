0da1a90fab69d6918fd4e197daee32ab4b3bec05
raptor72 <mctaras51@yandex.ru>
2022-03-21 22:36:42 +0400

    read by chanks

--- main.go
+++ main.go
@@ -1,34 +1,86 @@
 package main
 
 import (
+    "io"
     "os"
 	"fmt"
-    "io/ioutil"
-	"compress/gzip"
+    "log"
+    // "io/ioutil"
+    "strings"
+    "compress/gzip"
 	"github.com/bradfitz/gomemcache/memcache"	
 )
+
+
 
 func main() {
 	mc := memcache.New("127.0.0.1:11211")
     fmt.Println(mc)
 
-	file, err := os.Open("20170929000300.tsv.gz")
+    nBytes, nChunks := int64(0), int64(0)
+    file, err := os.Open("20170929000300.tsv.gz")
     if err != nil {
         fmt.Println(err)
     }
     defer file.Close()
+    ff := "ап"
+    fmt.Println(len([]byte(ff)))
 
-	fz, err := gzip.NewReader(file)
+	zipReader, err := gzip.NewReader(file)
 	if err != nil {
 	  fmt.Println(err)
 	}
-    defer fz.Close()
+    defer zipReader.Close()
 
-	s, err := ioutil.ReadAll(fz)
-    if err != nil {
-        fmt.Println(err)
+    buf := make([]byte, 0, 4*1024)
+
+    // corrected_bytes_len := 0
+
+    for {
+        n, err := zipReader.Read(buf[:cap(buf)])
+        // n = n + corrected_bytes_len
+
+        buf = buf[:n]
+
+        // fmt.Println(string(buf))
+        smass := strings.Split(string(buf), "\n")
+        fmt.Println(smass)
+        strings_in_batch := len(smass)
+        last_string := smass[strings_in_batch - 1]
+        fmt.Println(last_string)
+        fmt.Println(len([]byte(last_string)))
+        fmt.Println("#########################################")
+        // corrected_bytes_len = len([]byte(last_string))
+//        }
+//        fmt.Printf("%T", buf)
+//        s, err := reader.ReadString('\n')
+        if n == 0 {
+            if err == nil {
+                continue
+            }
+            if err == io.EOF {
+                break
+            }
+            log.Fatal(err)
+        }
+        nChunks++
+        nBytes += int64(len(buf))
+
+        // buf = buf[:n]
+        // process buf
+        if err != nil && err != io.EOF {
+            log.Fatal(err)
+        }
     }
-    fmt.Println(string(s))   
+    log.Println("Bytes:", nBytes, "Chunks:", nChunks)
+
+
+    
+	// s, err := ioutil.ReadAll(zipReader)
+    // if err != nil {
+    //     fmt.Println(err)
+    // }
+    // fmt.Println(string(s))   
 
 
 }

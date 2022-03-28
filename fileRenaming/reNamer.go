13ad5c137eb6904a87146233ad19a6767ee7cf0a
tarasov <tarasov@o-code.ru>
2022-03-28 09:20:16 +0400

    Latency emulator

--- fileRenaming/reNamer.go
+++ fileRenaming/reNamer.go
@@ -7,7 +7,19 @@ import (
 	"log"
     "strings"
     "sort"
+    "sync"
+	"time"
+    "math/rand"
 )
+
+
+
+func someLatency(Name string, w *sync.WaitGroup) { 
+    defer w.Done()
+	latency := rand.Intn(500) + 500
+	time.Sleep(time.Duration(latency) * time.Millisecond)
+	fmt.Println(Name)
+}
 
 
 
@@ -16,18 +28,21 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-    fmt.Println(filesFromDir)
+    // fmt.Println(filesFromDir)
     var targetFiles []os.FileInfo
-
+    wg := new(sync.WaitGroup)
 	for _, file := range filesFromDir {
         // if !strings.HasPrefix(file.Name(), ".") && strings.HasSuffix(file.Name(), ".tsv.gz") && file.Name() == "20170929000300.tsv.gz" {
 		if !strings.HasPrefix(file.Name(), ".") && strings.HasSuffix(file.Name(), ".tsv.gz") {
             targetFiles = append(targetFiles, file)
-			log.Printf("Type: %T, name: %s, size: %d\n", file, file.Name(), file.Size())
+			// log.Printf("Type: %T, name: %s, size: %d\n", file, file.Name(), file.Size())
+			sort.Slice(targetFiles, func(i, j int) bool { return targetFiles[i].Name() < targetFiles[j].Name() })
+			// sort.Slice(targetFiles, func(i, j int) bool { return targetFiles[i].ModTime().After(targetFiles[j].ModTime() )})
 		}
 	}
-    fmt.Println(targetFiles)
-	// sort.Slice(targetFiles, func(i, j int) bool { return targetFiles[i].Name() < targetFiles[j].Name() })
-	sort.Slice(targetFiles, func(i, j int) bool { return targetFiles[i].ModTime().After(targetFiles[j].ModTime() )})
-	fmt.Println(targetFiles)
+	for _, file := range targetFiles {
+		wg.Add(1)
+		go someLatency(file.Name(), wg)
+	}
+	wg.Wait()
 }

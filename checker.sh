ef1760331956818026fe0c6110e1572ffc0bf752
raptor72 <mctaras51@yandex.ru>
2022-04-13 22:55:40 +0400

    modified checker

--- checker.sh
+++ checker.sh
@@ -3,4 +3,8 @@ for file in $(ls -a | grep gz);
 do key=$(zcat $file | tail -n 1 | awk '{print $1, ":", $2}'|tr -d " ");
 echo "key: $key";
 echo "get $key" | nc -N 127.0.0.1 11211;
+r=$RANDOM;
+rkey=$(zcat $file | awk '{if (NR==var) print $1, ":", $2}' var="${r}"|tr -d " ");
+echo "random key line $r: $rkey";
+echo "get $rkey" | nc -N 127.0.0.1 11211;
 done

27021411b0e785703e97b1eac389669c298ee31e
raptor72 <mctaras51@yandex.ru>
2022-05-05 21:48:06 +0400

    Added more tests

--- memcLoader_test.go
+++ memcLoader_test.go
@@ -44,3 +44,74 @@ func TestCahingCorrectTrack(t *testing.T) {
      	}
 	}
 }
+
+
+func TestBadLenTrack(t *testing.T) {
+    expectedGoodCounter, expectedErrCounter := 0, 1
+	stringTrack := "dvid 8c4a88e21e2753663561b6f26792ef27 169.423781883 13.1686319629"
+	bytesTrack := []byte(stringTrack)
+	_, goodCounter, errCounter := parseBuff(bytesTrack)
+
+	if goodCounter != expectedGoodCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedGoodCounter, goodCounter)
+	}
+	if errCounter != expectedErrCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedErrCounter, errCounter)
+	}
+}
+
+func TestNotFloatLat(t *testing.T) {
+    expectedGoodCounter, expectedErrCounter := 0, 1
+	stringTrack := "dvid 8c4a88e21e2753663561b6f26792ef27 abc 13.1686319629 324324 34324"
+	bytesTrack := []byte(stringTrack)
+	_, goodCounter, errCounter := parseBuff(bytesTrack)
+
+	if goodCounter != expectedGoodCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedGoodCounter, goodCounter)
+	}
+	if errCounter != expectedErrCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedErrCounter, errCounter)
+	}
+}
+
+func TestLatMore180(t *testing.T) {
+    expectedGoodCounter, expectedErrCounter := 0, 1
+	stringTrack := "dvid 8c4a88e21e2753663561b6f26792ef27 180.0001 13.1686319629 324324 34324"
+	bytesTrack := []byte(stringTrack)
+	_, goodCounter, errCounter := parseBuff(bytesTrack)
+
+	if goodCounter != expectedGoodCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedGoodCounter, goodCounter)
+	}
+	if errCounter != expectedErrCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedErrCounter, errCounter)
+	}
+}
+
+func TestLonLessMinus180(t *testing.T) {
+    expectedGoodCounter, expectedErrCounter := 0, 1
+	stringTrack := "dvid 8c4a88e21e2753663561b6f26792ef27 179.0001 -183.1686319629 324324 34324"
+	bytesTrack := []byte(stringTrack)
+	_, goodCounter, errCounter := parseBuff(bytesTrack)
+
+	if goodCounter != expectedGoodCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedGoodCounter, goodCounter)
+	}
+	if errCounter != expectedErrCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedErrCounter, errCounter)
+	}
+}
+
+func TestDoubleError(t *testing.T) {
+    expectedGoodCounter, expectedErrCounter := 0, 1
+	stringTrack := "dvid 8c4a88e21e2753663561b6f26792ef27 198 -183.1686319629 324324 34324"
+	bytesTrack := []byte(stringTrack)
+	_, goodCounter, errCounter := parseBuff(bytesTrack)
+
+	if goodCounter != expectedGoodCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedGoodCounter, goodCounter)
+	}
+	if errCounter != expectedErrCounter {
+		t.Fatalf("Want %v\n, but got %v", expectedErrCounter, errCounter)
+	}
+}
